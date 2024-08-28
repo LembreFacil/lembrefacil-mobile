6cc39b52f87f9570b93004994901eb8e0934c566
Igor Oliveira <igornutrietec2020@gmail.com>
2024-08-28 09:12:49 -0300

    PROJETO BASE CRIADO
    Projeto base criado com telas iniciais(Splash Screen, Login e Cadastro) ainda não finalizadas

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,5 +1,5 @@
-import { LinearGradient } from 'expo-linear-gradient'; // Importar do Expo
-import { Image, View } from "native-base";
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image, Text, View } from "native-base";
 import { TEMAS } from "./estilos/temas";
 
 export default function Splash() {
@@ -8,34 +8,104 @@ export default function Splash() {
             style={{
                 flex: 1
             }}
-            colors={[TEMAS.colors.darkGreen, TEMAS.colors.lightGreen]} // Certifique-se de que essas cores estão disponíveis no seu tema
+            colors={[TEMAS.colors.darkGreen, TEMAS.colors.lightGreen]}
         >
-            <View style={{flex:1}}>
+            <View style={{ flex: 1 }}>
                 <Image
-                 source={require("./assets/hero2.png")}
-                 alt='Icone Idoso'
-                 style={{
-                    height: 70,
-                    width:70,
-                 }}
+                    source={require("./assets/hero2.png")}
+                    alt='Icone Idoso'
+                    style={{
+                        height: 100,
+                        width: 100,
+                        position: "absolute",
+                        top: 10,
+                        transform: [
+                            { translateX: 20 },
+                            { translateY: 50 },
+                            { rotate: "-15deg" }
+                        ]
+                    }}
                 />
-                
                 <Image
-                 source={require("./assets/hero1.png")}
-                 alt='Icone Idoso'
-                 style={{
-                    height: 170,
-                    width: 170,
+                    source={require("./assets/hero2.png")}
+                    alt='Icone Idoso'
+                    style={{
+                        height: 100,
+                        width: 100,
+                        position: "absolute",
+                        top: -30,
+                        left: 130,
+                        transform: [
+                            { translateX: 20 },
+                            { translateY: 50 },
+                            { rotate: "-10deg" }
+                        ]
+                    }}
+                />
+                <Image
+                    source={require("./assets/hero2.png")}
+                    alt='Icone Idoso'
+                    style={{
+                        height: 100,
+                        width: 100,
+                        position: "absolute",
+                        top: 160,
+                        left: -30,
+                        transform: [
+                            { translateX: 20 },
+                            { translateY: 50 },
+                            { rotate: "15deg" }
+                        ]
+                    }}
+                />
+                <Image
+                    source={require("./assets/hero1.png")}
+                    alt='Icone Idoso'
+                    style={{
+                        height: 170,
+                        width: 170,
+                        position: "absolute",
+                        top: 110,
+                        left: 120,
+                        transform: [
+                            { translateX: 50 },
+                            { translateY: 50 },
+                            { rotate: "-15deg" }
+                        ]
+                    }}
+                />
+                <View style={{
+                    paddingHorizontal: 20,
                     position: "absolute",
-                    top: 110,
-                    left: 120,
-                    transform: [
-                        {translateX: 50},
-                        {translateY: 50},
-                        {rotate: "-15deg"}
-                    ]
-                 }}
-                />
+                    top: 400, // Aumente este valor para mover o texto para baixo
+                    width: "100%",
+                }}>
+                    <Text style={{
+                        fontSize: 50,
+                        fontWeight: '800',
+                        paddingTop: 40, // Adiciona um espaço superior dentro do texto
+                        color: TEMAS.colors.white
+                    }}>Vamos</Text>
+                    <Text style={{
+                        fontSize: 46,
+                        fontWeight: '800',
+                        paddingTop: 42, // Adiciona um espaço superior dentro do texto
+                        color: TEMAS.colors.white
+                    }}>Começar!</Text>
+
+                    <View style={{ marginVertical: 22 }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: TEMAS.colors.white,
+                            width: "80%"
+                        }}>Nunca mais perca uma Consulta</Text>
+                        <Text style={{
+                            fontSize: 16,
+                            color: TEMAS.colors.white,
+                            width: "80%"
+                        }}>Nunca mais esqueça aquele Remédio</Text>
+                    </View>
+                </View>
             </View>
         </LinearGradient>
     );
