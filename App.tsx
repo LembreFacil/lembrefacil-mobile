b0239940ef62fac47853a68cc5915808543dec39
Igor Oliveira <igornutrietec2020@gmail.com>
2024-09-05 18:23:50 -0300

    NAVEGAÇÃO ENTRE TELAS FUNCIONANDO
    Navegação entre telas funcionando, porém ainda precisa ser criado botão para voltar a tela anterior.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
-import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { TEMAS } from './src/estilos/temas';
 import Login from './src/Login';
+import Splash from './src/Splash';
+import Cadastro from './src/Cadastro';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider theme={TEMAS}>
-      <StatusBar backgroundColor={TEMAS.colors.black}/>
-      <Login/>
+      <StatusBar backgroundColor={TEMAS.colors.black} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Cadastro" component={Cadastro} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
-
--- src/Login.tsx
+++ src/Login.tsx
@@ -8,7 +8,7 @@ import { EntradaDeTexto } from "./componentes/EntradaDeTexto";
 import SplashBtn from "./componentes/SplashBtn";
 import { TEMAS } from './estilos/temas';
 
-export default function Login() {
+export default function Login({ navigation }) {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
@@ -17,85 +17,107 @@ export default function Login() {
       style={{ flex: 1 }}
       colors={[TEMAS.colors.darkGreen, TEMAS.colors.lightGreen]}
     >
-      <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <View style={{ flex: 1, marginHorizontal: 22 }}>
-          <View style={{ marginVertical: 22 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ 
+          flex: 1, 
+          alignItems: 'center', 
+          justifyContent: "center",
+          paddingHorizontal: 22 
+        }}>
+          {/* Imagem no topo */}
+          {/* <View style={{ marginBottom: 20 }}>
+            <Image
+              source={require("./assets/logo.png")}
+              style={{
+                width: 200, // Ajuste o tamanho conforme necessário
+                height: 200, // Ajuste o tamanho conforme necessário
+                resizeMode: "contain", // Ajuste conforme necessário, pode ser "cover" ou "stretch"
+              }}
+              alt='Logo' // Adiciona uma descrição alternativa para a imagem
+            />
+          </View> */}
+
+          {/* Conteúdo */}
+          <View style={{ width: "100%" }}>
             <Text style={{
               fontSize: 22,
               fontWeight: "bold",
               marginVertical: 12,
-              color: TEMAS.colors.white
+              color: TEMAS.colors.white,
             }}>
               Que bom te ver aqui de novo!
             </Text>
             <Text style={{
               fontSize: 16,
-              color: TEMAS.colors.white
+              color: TEMAS.colors.white,
             }}>
               Nunca perca uma Consulta ou Remédio!
             </Text>
-          </View>
 
-          <EntradaDeTexto
-            label="Nome"
-            placeholder="Insira seu nome"
-          />
-
-          <View style={{ marginBottom: 12, marginTop: 8 }}>
-            <Text style={{
-              fontSize: 16,
-              fontWeight: "500",
-              marginVertical: 8,
-              color: TEMAS.colors.white
-            }}>
-              Senha
-            </Text>
-            <View style={{
-              width: "100%",
-              height: 48,
-              borderColor: TEMAS.colors.black,
-              backgroundColor: TEMAS.colors.white,
-              borderWidth: 1,
-              borderRadius: 8,
-              alignItems: "center",
-              justifyContent: "center",
-              paddingLeft: 22
-            }}>
-              <TextInput
-                placeholder='Insira sua Senha'
-                placeholderTextColor={TEMAS.colors.black}
-                secureTextEntry={isPasswordShown}
-                style={{
-                  width: "100%",
-                  fontSize: 16
-                }}
-              />
-              <TouchableOpacity 
-                onPress={() => setIsPasswordShown(!isPasswordShown)}
-                style={{ position: "absolute", right: 12 }}
-              >
-                {isPasswordShown ? (
-                  <Ionicons name="eye-off" size={24} color={TEMAS.colors.black} />
-                ) : (
-                  <Ionicons name="eye" size={24} color={TEMAS.colors.black} />
-                )}
-              </TouchableOpacity>
-            </View>
-          </View>
-
-          <SplashBtn
-            titleSplashBtn="Entrar"
-            style={{ marginTop: 18 }}
-          />
-
-          <View style={{ flexDirection: "row", alignItems: "center", marginVertical: 6, marginTop: 10 }}>
-            <Checkbox
-              isChecked={isChecked}
-              onChange={() => setIsChecked(!isChecked)}
-              color={TEMAS.colors.black}
-              style={{ marginRight: 8, marginTop: 10 }}
+            <EntradaDeTexto
+              label="Email"
+              placeholder="Insira seu email"
             />
-            <Text style={{ color: TEMAS.colors.white }}>Lembrar Acesso</Text>
+
+            <View style={{ marginBottom: 12, marginTop: 8 }}>
+              <Text style={{
+                fontSize: 16,
+                fontWeight: "500",
+                marginVertical: 8,
+                color: TEMAS.colors.white
+              }}>
+                Senha
+              </Text>
+              <View style={{
+                width: "100%",
+                height: 48,
+                borderColor: TEMAS.colors.black,
+                backgroundColor: TEMAS.colors.white,
+                borderWidth: 1,
+                borderRadius: 8,
+                alignItems: "center",
+                justifyContent: "center",
+                paddingLeft: 22
+              }}>
+                <TextInput
+                  placeholder='Insira sua Senha'
+                  placeholderTextColor={TEMAS.colors.black}
+                  secureTextEntry={isPasswordShown}
+                  style={{
+                    width: "100%",
+                    fontSize: 16
+                  }}
+                />
+                <TouchableOpacity 
+                  onPress={() => setIsPasswordShown(!isPasswordShown)}
+                  style={{ position: "absolute", right: 12 }}
+                >
+                  {isPasswordShown ? (
+                    <Ionicons name="eye-off" size={24} color={TEMAS.colors.black} />
+                  ) : (
+                    <Ionicons name="eye" size={24} color={TEMAS.colors.black} />
+                  )}
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <SplashBtn
+              titleSplashBtn="Entrar"
+              style={{ marginTop: 18 }}
+              onPress={() => navigation.navigate('Home')}
+            />
+
+            <View style={{ flexDirection: "row", alignItems: "center", marginVertical: 6, marginTop: 20 }}>
+              <Checkbox
+                value="lembrarAcesso"
+                isChecked={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+                accessibilityLabel="Lembrar Acesso"
+                colorScheme="light"
+                style={{ marginRight: 8 }}
+              />
+              <Text style={{ color: TEMAS.colors.white }}>Lembrar Acesso</Text>
+            </View>
           </View>
         </View>
       </SafeAreaView>
